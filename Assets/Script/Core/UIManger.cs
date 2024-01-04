1b49f41bd6aef3dec4f92257313fd1a6be0ba26a
A-Dyu <adyu1121>
2024-01-04 10:11:57 +0900

    xs

--- Assets/Script/Core/UIManger.cs
+++ Assets/Script/Core/UIManger.cs
@@ -94,6 +94,7 @@ public class UIManger : Singleton<UIManger>
                 {
                     HideText();
 
+                    if(_textData.Event != 0)
                     Eventbus.EventInvoke(_textData.Event);
                 }
                 else
